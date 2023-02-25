30816d34cee20b8cb46d1352b225a9a28b25a371
Volodimir Slobodyanyuk <volodimir@gmail.com>
2023-02-25 00:25:06 -0800

    Add files via upload

---
+++ include/global_main.h
@@ -0,0 +1,25 @@
+/*
+ * global_main.h
+ *
+ *  Created on: Feb 12, 2023
+ *      Author: voldemaro
+ */
+
+#ifndef INCLUDE_GLOBAL_MAIN_H_
+#define INCLUDE_GLOBAL_MAIN_H_
+
+extern "C"
+{
+#include "parameters.h"
+#include "configs.h"
+#include "objects.h"
+#include "threads.h"
+#include "profiler.h"
+}
+
+#include <string.h>
+#include <getopt.h>
+#include <time.h>
+#include <signal.h>
+
+#endif /* INCLUDE_GLOBAL_MAIN_H_ */
